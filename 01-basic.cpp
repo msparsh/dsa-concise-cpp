9ff6085510f3477b6274d8bfb0f3a283b111349d
Sparsh Mishra <avidunable@gmail.com>
2025-10-01 01:43:27 +0530

    Edited 01 and added 02

--- 01-basic.cpp
+++ 01-basic.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
 
-int main(){
+int add(int a, int b);
+
+int main() {
     int a = 5;
     char b = 'B';
     bool c = true;
     std::string d = "Hello, World!";
+    bool e = false;
 
     std::cout << "everything : " << a << b << c << d << std::endl;
 
-    std::cout<< "DONE DONE DONE" << std::endl;
+    if (a <= 5) {
+        std::cout << "a is leq 5" << std::endl;
+    }
+    else {
+        std::cout << "a is greater than 5";
+    }
+
+    if (c) {
+        std::cout << "c is true" << std::endl;
+    }
+    if (e) {
+        std::cout << "e is true" << std::endl;
+    }
+    else if (not e) {
+        std::cout << "e is not true" << std::endl;
+    }
+
+    for (int i = 0; i < 10; i = i + 2) {
+        std::cout << i << "-_-";
+    }
+    std::cout << std::endl;
+
+    std::cout << "10 + 20 is " << add(10, 20) << std::endl;
+
+    int l[5] = {10, 100, 1000, 10000, 100000};
+    int *ptr_of_first = &l[0];
+
+    std::cout << l[0] << std::endl;
+    std::cout << *ptr_of_first << std::endl;
+
+    std::cout << "DONE DONE DONE" << std::endl;
+}
+
+int add(int a, int b) {
+    return a + b;
 }
---
+++ 02-array-vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int arr_primes[6] = {2, 3, 5, 7, 11, 13};
+    int arr_evens[7] = {2, 4, 6, 8, 10, 12, 14};
+
+    std::cout << arr_evens[2]<<arr_primes[3]<<std::endl;
+
+}
