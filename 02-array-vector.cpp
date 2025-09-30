25f79fe4f622ad99944a2b2433c198f609703e98
Sparsh Mishra <avidunable@gmail.com>
2025-10-01 01:50:34 +0530

    Updated 02

--- 02-array-vector.cpp
+++ 02-array-vector.cpp
@@ -6,6 +6,19 @@ int main()
     int arr_primes[6] = {2, 3, 5, 7, 11, 13};
     int arr_evens[7] = {2, 4, 6, 8, 10, 12, 14};
 
-    std::cout << arr_evens[2]<<arr_primes[3]<<std::endl;
+    std::cout << arr_evens[2] << arr_primes[3] << std::endl;
+
+    std::vector<int> primes = {2, 3, 5, 7, 11, 13};
+    primes.push_back(17);
+
+    std::cout << "primes[0]: " << primes.front() << std::endl;
+    std::cout << "primes[-1]: " << primes.back() << std::endl;
+
+    std::cout << "Size of primes: " << primes.size() << std::endl;
+    for (const int& p:primes){
+        std::cout<<p<<"-";
+    }
+    std::cout<<std::endl;
+
 
 }
