4152a81f5c3b811150e54d8aaf00e618aaa726a4
Sparsh Mishra <avidunable@gmail.com>
2025-10-01 01:14:39 +0530

    Added 01

---
+++ 01-basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int a = 5;
+    char b = 'B';
+    bool c = true;
+    std::string d = "Hello, World!";
+
+    std::cout << "everything : " << a << b << c << d << std::endl;
+
+    std::cout<< "DONE DONE DONE" << std::endl;
+}
